b03c227e0c5c3145a23244f8858ee164792494d8
AlexCrux0407 <130202688+AlexCrux0407@users.noreply.github.com>
2025-02-13 17:02:43 -0600

    endpoint 2

--- POSTMAN/entornoTareas/Main.py
+++ POSTMAN/entornoTareas/Main.py
@@ -14,6 +14,13 @@ tareas = [
         "descripcion": "Entregar la tarea de Moya",
         "vencimiento": "12/02/2025",
         "estado": "pendiente"
+    },
+    {
+        "id": 2,
+        "titulo": "Entrega Pons",
+        "descripcion": "Entregar la tarea de Pons",
+        "vencimiento": "15/02/2025",
+        "estado": "pendiente"
     }
 ]
 
@@ -21,3 +28,12 @@ tareas = [
 @app.get("/tareas", tags=["tareas"])
 def mostrarTareas():
     return {"Las tareas registradas son": tareas}
+
+#buscar tarea por ID
+@app.get("/tareas/{id}", tags=["tareas"])
+
+def buscarTarea(id: int):
+    for tarea in tareas:
+        if tarea["id"] == id:
+            return tarea
+    raise HTTPException(status_code=404, detail="Tarea no encontrada")
