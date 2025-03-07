9e298780a42820dd60e87f3bffd023ea84d27610
AlexCrux0407 <130202688+AlexCrux0407@users.noreply.github.com>
2025-03-06 20:41:42 -0600

    final pt6

--- FAST/entornoFast/Scripts/activate.bat
+++ FAST/entornoFast/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Alexis Cruz\Desktop\UPQ\8vo cuatri\TAI\TAI192\FAST\entornoFast
+set VIRTUAL_ENV=C:\Users\Alexis Cruz\Desktop\UPQ\8vocuatri\TAI192\FAST\entornoFast
 
 if not defined PROMPT set PROMPT=$P$G
 
--- FAST/main.py
+++ FAST/main.py
@@ -1,57 +1,55 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.responses import JSONResponse
-from typing import Optional,List
-from modelsPydantic import modeloUsuario,modeloAuth
+from typing import Optional, List, Dict
+
+from modelsPydantic import modeloUsuario, modeloAuth
 from genToken import createToken
+from middlewares import BearerJWT
+
 app = FastAPI(
     title="Mi primer API 192",
     description="Esto es una descripción de mi API",
     version="1.0.1"
 )
 
-
-    
-
-usuarios=[
-    {"id":1, "nombre":"Alexis", "edad":21,"correo":"alexis@gmail.com"},
-    {"id":2, "nombre":"Jose", "edad":25,"correo":"jose@gmail.com"},
-    {"id":3, "nombre":"Maria","edad":27,"correo":"maria@gmail.com"},
-    {"id":4, "nombre":"Pedro","edad":23,"correo":"pedrp@gmail.com"}
+usuarios = [
+    {"id": 1, "nombre": "Alexis", "edad": 21, "correo": "alexis@gmail.com"},
+    {"id": 2, "nombre": "Jose", "edad": 25, "correo": "jose@gmail.com"},
+    {"id": 3, "nombre": "Maria", "edad": 27, "correo": "maria@gmail.com"},
+    {"id": 4, "nombre": "Pedro", "edad": 23, "correo": "pedrp@gmail.com"}
 ]
 
-#endpoint home
+# endpoint home
 @app.get("/", tags=["Hola mundo"])
-
 def home():
     return {"hello": "world FastAPI"}
 
-#endpoint consulta todos
-@app.get("/todosUsuarios",response_model=List[modeloUsuario], tags=["Operaciones CRUD"])
+# endpoint consulta todos
+@app.get("/todosUsuariox", dependencies=[Depends(BearerJWT())], response_model=List[modeloUsuario], tags=["Operaciones CRUD"])
 def leerUsuarios():
     return usuarios
 
-#endpoint Agregar nuevos usuarios
-@app.post("/usuarios/",response_model=modeloUsuario, tags=["Operaciones CRUD"])
+# endpoint Agregar nuevos usuarios
+@app.post("/usuarios/", dependencies=[Depends(BearerJWT())], response_model=modeloUsuario, tags=["Operaciones CRUD"])
 def agregarUsuario(usuario: modeloUsuario):
     for usr in usuarios:
         if usr["id"] == usuario.id:
             raise HTTPException(status_code=400, detail="El ID ya existe")
-    usuarios.append(usuario)
-    return {"El usuario se agregó correctamente": usuario}
+    usuarios.append(usuario.model_dump())
+    return usuario
 
-#endpoint Actualizar un usuario
-@app.put("/usuarios/{id}",response_model=modeloUsuario, tags=["Operaciones CRUD"])
+# endpoint Actualizar un usuario
+@app.put("/usuarios/{id}", dependencies=[Depends(BearerJWT())], response_model=modeloUsuario, tags=["Operaciones CRUD"])
 def actualizarUsuario(id: int, usuario: modeloUsuario):
-    for index,usr in enumerate(usuarios):
+    for index, usr in enumerate(usuarios):
         if usr["id"] == id:
             usuarios[index] = usuario.model_dump()
-            return usuarios[index] 
+            return usuarios[index]
 
     raise HTTPException(status_code=400, detail="El usuario no existe")
 
-#endpoint Eliminar un usuario
-
-@app.delete("/usuarios/{id}", tags=["Operaciones CRUD"])
+# endpoint Eliminar un usuario
+@app.delete("/usuarios/{id}", dependencies=[Depends(BearerJWT())], tags=["Operaciones CRUD"])
 def eliminarUsuario(id: int):
     for i, usr in enumerate(usuarios):
         if usr["id"] == id:
@@ -59,12 +57,12 @@ def eliminarUsuario(id: int):
             return {"El usuario se eliminó correctamente": {"id": id}}
     raise HTTPException(status_code=404, detail="El usuario no existe")
 
-#endpoint autenticacion
-@app.post("/auth", response_model =modeloAuth, tags=["Autentificación"])
+# endpoint autenticacion
+@app.post("/auth", tags=["Autentificación"])
 def login(autorizacion: modeloAuth):
     if autorizacion.correo == "alexis@gmail.com" and autorizacion.passw == "admin123":
         token: str = createToken(autorizacion.model_dump())
         print(token)
-        return JSONResponse(token)
+        return {"access_token": token, "token_type": "bearer"}
     else:
         raise HTTPException(status_code=400, detail="Usuario sin acceso")
