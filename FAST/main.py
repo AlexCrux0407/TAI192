47af603945cfd348c4ead2d6838349ad0b0a9765
AlexCrux0407 <130202688+AlexCrux0407@users.noreply.github.com>
2025-03-25 20:10:59 -0600

    pt8

--- FAST/main.py
+++ FAST/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from typing import Optional, List, Dict
 
 from modelsPydantic import modeloUsuario, modeloAuth
@@ -17,10 +18,10 @@ app = FastAPI(
 Base.metadata.create_all(bind=engine)
     
 usuarios = [
-    {"id": 1, "nombre": "Alexis", "edad": 21, "correo": "alexis@gmail.com"},
-    {"id": 2, "nombre": "Jose", "edad": 25, "correo": "jose@gmail.com"},
-    {"id": 3, "nombre": "Maria", "edad": 27, "correo": "maria@gmail.com"},
-    {"id": 4, "nombre": "Pedro", "edad": 23, "correo": "pedrp@gmail.com"}
+    {"id": 1, "nombre": "Alexis", "edad": 21, "email": "alexis@gmail.com"},
+    {"id": 2, "nombre": "Jose", "edad": 25, "email": "jose@gmail.com"},
+    {"id": 3, "nombre": "Maria", "edad": 27, "email": "maria@gmail.com"},
+    {"id": 4, "nombre": "Pedro", "edad": 23, "email": "pedrp@gmail.com"}
 ]
 
 # endpoint home
@@ -29,18 +30,45 @@ def home():
     return {"hello": "world FastAPI"}
 
 # endpoint consulta todos
-@app.get("/todosUsuarios", dependencies=[Depends(BearerJWT())], response_model=List[modeloUsuario], tags=["Operaciones CRUD"])
+@app.get("/todosUsuarios", response_model=List[modeloUsuario], tags=["Operaciones CRUD"])
 def leerUsuarios():
-    return usuarios
+    db = Session()
+    try:
+        consulta = db.query(User).all()
+        return JSONResponse(content=jsonable_encoder(consulta))
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"message":"error al consultar usuarios","Excepcion": str(e)})
+    finally:
+        db.close()
 
+#endpoint buscar por id
+@app.get("/usuarios/{id}", response_model=modeloUsuario, tags=["Operaciones CRUD"])
+def leerUsuario(id: int):
+    db = Session()
+    try:
+        consultauno = db.query(User).filter(User.id == id).first()
+        if not consultauno:
+            return JSONResponse(status_code=404, content={"message":"Usuario no encontrado"})
+        return JSONResponse(content=jsonable_encoder(consultauno))
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"message":"error al consultar usuario","Excepcion": str(e)})
+    finally:
+        db.close()
+        
+        
 # endpoint Agregar nuevos usuarios
 @app.post("/usuarios/", dependencies=[Depends(BearerJWT())], response_model=modeloUsuario, tags=["Operaciones CRUD"])
 def agregarUsuario(usuario: modeloUsuario):
-    for usr in usuarios:
-        if usr["id"] == usuario.id:
-            raise HTTPException(status_code=400, detail="El ID ya existe")
-    usuarios.append(usuario.model_dump())
-    return usuario
+    db = Session()
+    try:
+        db.add(User(**usuario.model_dump()))
+        db.commit()
+        return JSONResponse (status_code=201, content={"message": "Usuario creado correctamente", "usuario": usuario.model_dump()})
+    except Exception as e:
+        db.rollback()
+        return JSONResponse(status_code=500, content={"message":"error al guardar usuario","Excepcion": str(e)})
+    finally:
+        db.close()
 
 # endpoint Actualizar un usuario
 @app.put("/usuarios/{id}", dependencies=[Depends(BearerJWT())], response_model=modeloUsuario, tags=["Operaciones CRUD"])
@@ -64,7 +92,7 @@ def eliminarUsuario(id: int):
 # endpoint autenticacion
 @app.post("/auth", tags=["Autentificación"])
 def login(autorizacion: modeloAuth):
-    if autorizacion.correo == "alexis@gmail.com" and autorizacion.passw == "admin123":
+    if autorizacion.email == "alexis@gmail.com" and autorizacion.passw == "admin123":
         token: str = createToken(autorizacion.model_dump())
         print(token)
         return {"access_token": token, "token_type": "bearer"}
